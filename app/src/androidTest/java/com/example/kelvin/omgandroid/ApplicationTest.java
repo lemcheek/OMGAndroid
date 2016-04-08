4f7a0948b1e32d35d322ff51987f6adaced81aac
lemcheek <lemcheek@yahoo.com>
2016-04-08 17:21:03 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/kelvin/omgandroid/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.kelvin.omgandroid;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest(String myProperty) {
+        super(Application.class);
+    }
+}
+
+
